23782de4a234daac7da79015563b7b8e16d1cb01
KarolKwarciak <kakwarc@gmail.com>
2024-11-12 21:06:41 +0100

    Prostokąty

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -6,8 +6,7 @@ namespace Simulator
     {
         static void Main(string[] args)
         {
-            Lab4a();
-            Lab4b();
+            Lab5a();
         }
 
         static void Lab4a()
@@ -56,6 +55,42 @@ namespace Simulator
             foreach (var o in myObjects)
             {
                 Console.WriteLine(o);
+            }
+        }
+        static void Lab5a()
+        {
+            try
+            {
+                var rect1 = new Rectangle(5, 6, 2, 2);
+                Console.WriteLine(rect1);
+                var rect2 = new Rectangle(2, 6, 5, 2);
+                Console.WriteLine(rect2);
+                try
+                {
+                    var rect3 = new Rectangle(2, 2, 2, 6);
+                    Console.WriteLine(rect3);
+                }
+                catch (ArgumentException expectation)
+                {
+                    Console.WriteLine($"{expectation.Message}");
+                }
+                var p1 = new Point(2, 2);
+                var p2 = new Point(5, 6);
+                var rect4 = new Rectangle(p1, p2);
+                Console.WriteLine(rect4);
+
+                var pointInside = new Point(3, 3);
+                var pointOutside = new Point(1, 1);
+                var pointOnEdge = new Point(2, 6);
+                var pointOnEdge2 = new Point(5, 2);
+                Console.WriteLine($"Point {pointInside} inside the rectangle? {rect4.Contains(pointInside)}");
+                Console.WriteLine($"Point {pointOutside} inside the rectangle? {rect4.Contains(pointOutside)}");
+                Console.WriteLine($"Point {pointOnEdge} inside the rectangle? {rect4.Contains(pointOnEdge)}");
+                Console.WriteLine($"Point {pointOnEdge2} inside the rectangle? {rect4.Contains(pointOnEdge2)}");
+            }
+            catch (Exception expectation)
+            {
+                Console.WriteLine($"{expectation.Message}");
             }
         }
     }
